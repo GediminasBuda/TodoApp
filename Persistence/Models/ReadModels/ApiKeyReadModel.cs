9490ba0e78e7fadb5fb15ab3325e780f99b0ec70
Gediminas <gediminas.buda1@gmail.com>
2021-09-15 19:44:11 +0300

    Added ApiKeyReadModel

---
+++ Persistence/Models/ReadModels/ApiKeyReadModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Persistence.Models.ReadModels
+{
+    public class ApiKeyReadModel
+
+    { 
+        public Guid Id { get; set; }
+
+        public string ApiKey { get; set; }
+
+        public string UserId { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+    }
+}
